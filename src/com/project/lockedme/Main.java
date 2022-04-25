914ed69a11146d5e7db57b4a609076674c4726e9
8874-6 <pranjalmishra887462@gmail.com>
2022-04-25 16:36:26 +0530

    initial commit

---
+++ src/com/project/lockedme/Main.java
@@ -0,0 +1,13 @@
+package com.project.lockedme;
+
+public class Main {
+public static final String path = "E:\\ECLIPSE IDE\\Pranjal"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+}
+
